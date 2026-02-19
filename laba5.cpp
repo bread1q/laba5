8763e868d4ebeffab9cdb9a268f58584798df883
bread1q <osnova326@gmail.com>
2026-02-19 21:49:18 +0500

    Перекрываемые и наследуемые методы

--- laba5.cpp
+++ laba5.cpp
@@ -1,3 +1,31 @@
 #include <iostream>
 
 using namespace std;
+
+class Base {
+public:
+    // обычный метод
+    void method() {
+        cout << "Base::method()" << endl;
+    }
+};
+
+class Derived : public Base {
+public:
+    // перекрываемый метод
+    void method() {
+        cout << "Derived::method()" << endl;
+    }
+};
+
+int main() {
+    cout << endl << "=== Демонстрация перекрываемого и наследуемого методов ===" << endl << endl;
+    Base* baseptr = new Derived();
+    Derived* derivedptr = new Derived();
+
+    cout << "Вызов через указатель на Base: " << endl;
+    baseptr->method(); // Вызовется Base::method() - наследуемый метод
+
+    cout << "Вызов через указатель на Derived: " << endl;
+    derivedptr->method(); // Вызовется Derived::method() - перекрытый метод
+}
